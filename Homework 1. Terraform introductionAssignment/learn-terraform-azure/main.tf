382294dbcad9d3f8a57e043356048e060b553c6a
Artem Avetian <artemiustas@gmail.com>
2023-05-05 17:34:15 +0300

    changes

--- Homework 1. Terraform introductionAssignment/learn-terraform-azure/main.tf
+++ Homework 1. Terraform introductionAssignment/learn-terraform-azure/main.tf
@@ -1,20 +1,19 @@
-# Configure the Azure provider
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
   }
-
-  required_version = ">= 1.1.0"
+  cloud {
+    organization = "1250kv"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
 }
 
 provider "azurerm" {
   features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
 }
